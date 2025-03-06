00c80e35cd4da63ef18b66bff81ca59c60471c9c
zy99978455-otw <17701139970@163.com>
2025-03-06 10:00:00 +0800

    chore: add .env support and gitignore for security

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-	// 连接Cloudflare 的公共节点
-	client, err := ethclient.Dial("https://cloudflare-eth.com")
+	// 加载 .env 文件
+	_ = godotenv.Load()
+
+	// 从环境变量中获取 URL
+	nodeURL := os.Getenv("ETH_NODE_URL")
+    if nodeURL == "" {
+        log.Fatal("Error: ETH_NODE_URL is not set in .env or environment variables")
+    }
+
+	// 连接主节点
+	client, err := ethclient.Dial(nodeURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to Ethereum via Infura (loaded from .env)")
+
+	// 获取最新的区块号
+	blockNumber, err := client.BlockNumber(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Success! Connected to Ethereum Mainnet")
-	_ = client
+	fmt.Printf("Latest Block Number: %d\n", blockNumber)
 }
